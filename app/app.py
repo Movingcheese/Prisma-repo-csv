e803918b6d73b63584d111e64d843e2c0acd803e
David Oyebanji <davidoyebanji@outlook.com>
2024-08-07 17:07:37 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,6 @@ import json
 import ssl
 import urllib3
 
-
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def login_to_prisma_cloud(login_url, access_key, secret_key):
@@ -28,7 +27,7 @@ def login_to_prisma_cloud(login_url, access_key, secret_key):
             print(f"Response: {e.response.text}")
         return None
 
-def get_repos_from_prisma_cloud(api_url, jwt_token):
+def get_github_enterprise_repos(api_url, jwt_token):
     headers = {
         'content-type': 'application/json',
         'x-redlock-auth': jwt_token
@@ -47,7 +46,7 @@ def get_repos_from_prisma_cloud(api_url, jwt_token):
 
 def generate_csv(repos, output_file):
     if not repos:
-        print("No repositories to write to CSV.")
+        print("Response: No GitHub Enterprise repositories to write to CSV.")
         return
 
     fieldnames = repos[0].keys()
@@ -60,25 +59,24 @@ def generate_csv(repos, output_file):
 
     print(f"CSV file '{output_file}' has been generated successfully.")
 
-# APII login
+# API 
 BASE_URL = "https://api3.prismacloud.io"
 API_URL = f"{BASE_URL}/code/api/v1"
 LOGIN_URL = f"{BASE_URL}/login"
 ACCESS_KEY = "d9071b9f-8a3d-4f5f-87e8-ebc4fe857518"
 SECRET_KEY = "3taQez3V1Na7X3HE0lPRK53JeBs="
-OUTPUT_FILE = "prisma_cloud_repos.csv"
+OUTPUT_FILE = "repos.csv"
 
 
 jwt_token = login_to_prisma_cloud(LOGIN_URL, ACCESS_KEY, SECRET_KEY)
 
 if jwt_token:
-    print("Authentication successful. Proceeding to fetch repositories.")
-    
-    repos = get_repos_from_prisma_cloud(API_URL, jwt_token)
-    if repos:
-        print(f"Found {len(repos)} repositories.")
-        generate_csv(repos, OUTPUT_FILE)
+    print("Authentication successful. Proceeding to fetch GitHub Enterprise repositories.")
+    github_enterprise_repos = get_github_enterprise_repos(API_URL, jwt_token)
+    if github_enterprise_repos:
+        print(f"Found {len(github_enterprise_repos)} GitHub Enterprise repositories.")
+        generate_csv(github_enterprise_repos, OUTPUT_FILE)
     else:
-        print("Failed to retrieve repositories.")
+        print("No GitHub Enterprise repositories found or failed to retrieve repositories.")
 else:
     print("Failed to obtain JWT token. Cannot proceed with fetching repositories.")
